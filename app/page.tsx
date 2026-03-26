f91707d7fe1cc23c3396da19e714108b08e3a9ed
sikander-nawaz <sikandernawaz295@gmail.com>
2026-03-26 12:27:59 +0500

    start

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,21 @@
-import Hero from '@/components/Hero'
-import AboutSection from '@/components/AboutSection'
-import ExperienceSection from '@/components/ExperienceSection'
-import VolunteerCertificatesSection from '@/components/VolunteerCertificatesSection'
-import AnimatedSkills from '@/components/AnimatedSkills'
-import ProjectsSection from '@/components/ProjectsSection'
-import ContactSection from '@/components/ContactSection'
+import Hero from "@/components/Hero";
+import AboutSection from "@/components/AboutSection";
+// import ExperienceSection from '@/components/ExperienceSection'
+import VolunteerCertificatesSection from "@/components/VolunteerCertificatesSection";
+import AnimatedSkills from "@/components/AnimatedSkills";
+import ProjectsSection from "@/components/ProjectsSection";
+import ContactSection from "@/components/ContactSection";
 
 export default function Page() {
-    return (
-        <>
-            <Hero />
-            <AboutSection />
-            {/* <ExperienceSection /> */}
-            <VolunteerCertificatesSection />
-            <AnimatedSkills />
-            <ProjectsSection />
-            <ContactSection />
-        </>
-    )
+  return (
+    <>
+      <Hero />
+      <AboutSection />
+      {/* <ExperienceSection /> */}
+      <VolunteerCertificatesSection />
+      <AnimatedSkills />
+      <ProjectsSection />
+      <ContactSection />
+    </>
+  );
 }
